df78827cc5d72c3b9182a67b1adf201df55c7bd4
elvin <elvin@mangatoken.org>
2023-03-29 21:27:23 +0800

    fix src & srcset

--- view/overlay-page/render.js
+++ view/overlay-page/render.js
@@ -14,16 +14,15 @@ $(window).on('load', function() {
         let photoID = $(this).attr("src").split('/').pop().split('_', 1).join();
 
         $(".cosplayer-latest").append(`
-            <a href="#" class="cosplayer-link w-inline-block openPhotoOverlay" style="background-image:url('` + pictureResize(500, $(this).attr("src")) + `')" id="` + $(this).attr("src").split('/').pop().split('_', 1) + `">
+            <a href="#" class="cosplayer-link w-inline-block openPhotoOverlay" style="background-image:url('` + pictureResize(500, $(this).attr("src")) + `')" id="` + $(this).attr("src").split('/').pop().split('_', 1) + `" data-src="` + $(this).attr("src") + `" data-srcset="` + $(this).attr("srcset") + `">
                 <div class="cosplay-link-inner-block">
                     <div class="vote-count-block">
                         <img src="https://uploads-ssl.webflow.com/6279c7595e56e5a596e3bafb/641d37ed93f70c7e6625413e_clapping.png" loading="lazy" alt="" class="vote-count-img">
-                        <div class="vote-count-number photoID-` + photoID + `">` + getPictureVoteCount($(".cosplayerID").attr("id"), photoID) + `</div>
+                        <div class="vote-count-number photoID-` + photoID + `" >` + getPictureVoteCount($(".cosplayerID").attr("id"), photoID) + `</div>
                     </div>
                 </div>
             </a>
         `);
-        $(this).attr("src")
         $(this).remove();
     });
 
@@ -35,8 +34,7 @@ $(window).on('load', function() {
         //Unhide photo overlay layer
         $("#photo-overlay").removeClass("hidden");
         //Replace default image with idol img in photo overlay
-        let tempOriginalPicUrl = pictureOriginalSize($(this).attr("style").slice(22, -2));
-        $(".photo-column").children("img").attr("src", tempOriginalPicUrl).attr("loading","lazy").attr("alt","").attr("sizes","(max-width: 767px) 96vw, (max-width: 991px) 541px, 700px").attr("srcset", pictureResize(500,tempOriginalPicUrl) + " 500w, " + pictureResize(800,tempOriginalPicUrl) + " 800w, " + tempOriginalPicUrl);
+        $(".photo-column").children("img").attr("src", $(this).attr("data-src")).attr("srcset", $(this).attr("data-srcset")).attr("loading","lazy");
         //Replace count number with idol img vote count
         $(".photo-vote-count").empty().append($(this).find(".vote-count-number").text());
         //Start voting overlay
