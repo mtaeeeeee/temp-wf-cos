c3325a731d4f0d14160392e633e8ea254bcc87b4
MTae <flypy2902@gmail.com>
2023-04-16 11:24:50 +0700

    feat: get and update cosplayer detail

--- scripts/cosplayer.js
+++ scripts/cosplayer.js
@@ -1,3 +1,4 @@
+import { auth } from 'https://raw.githack.com/mtaeeeeee/temp-wf-cos/main/services/firebase.js'
 import { apiUrl } from 'https://raw.githack.com/mtaeeeeee/temp-wf-cos/main/utils/constant.js'
 
 const collectionId = '63ce4425b086d45e60078410'
@@ -21,4 +22,49 @@ export const createNewCosplayer = data => {
       console.error(errorThrown)
     }
   })
+}
+
+export const updateCosplayerDetail = (itemId, data) => {
+  $.ajax({
+    url: `${apiUrl}/collections/${collectionId}/items/${itemId}`,
+    method: 'PATCH',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      fields: {
+        ...data
+      }
+    }),
+    success: function (data) {
+      console.log(data)
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error(errorThrown)
+    }
+  })
+}
+
+export const getCosplayerDetail = async () => {
+  if (auth && auth.currentUser) {
+    const cosplayerId = auth.currentUser.uid
+
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: `${apiUrl}/collections/${collectionId}/items`,
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+          if (data && data.items) {
+            const cosplayer = data.items.find(cos => cos['cosplayer-id'] === cosplayerId)
+            return resolve(cosplayer)
+          }
+          return resolve(null)
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          return reject(errorThrown)
+        }
+      })
+    })
+  }
+
+  return Promise.resolve(null)
 }
